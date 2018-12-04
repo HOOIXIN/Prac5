e932541be9afb02f1d1ae16247980ac815b22d7b
Student <Student@PC-76>
2018-12-04 09:35:17 +0800

    new

---
+++ Prac5Test/src/prac5test/Prac5Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prac5test;
+
+/**
+ *
+ * @author Student
+ */
+public class Prac5Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello Travis");
+        System.out.print("Plz build successful lahh");
+    }
+    
+}
